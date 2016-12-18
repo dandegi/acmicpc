6fe890986f5e510f4e5a2682ca983bfcd4c24843
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2016-12-18 22:18:08 +0900

    주몽 해석후 따른거

--- 1940.java
+++ 1940.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] array = new int[N];
-		int M = sc.nextInt();
-		int sum = 0;
-		for (int i=0; i<N; i++) {
-			int Z = sc.nextInt();
-			array[i] = Z;
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int [] array = new int[10000001];
+		
+		for (int i=0; i<n; i++) {
+			array[sc.nextInt()]++;
 		}
-		int j = 0;
-		for (int i=0; i<N; i++) {
-			for (j=i+1; j<N; j++) {
-				if(array[i]+array[j] == M) {
-					sum += 1;
+		
+		int start = 1, end = m-1, count = 0;
+		
+		while (start<end) {
+			if (array[start] != 0 && array[end] != 0) {
+				if (start+end == m) {
+					count++;
 				}
 			}
+			start++;
+			end--;
 		}
-		System.out.println(sum);
+		System.out.println(count);
 	}
 }
