ca498125794bd476cbddacdfb664147775dba22f
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2016-12-19 01:51:28 +0900

    두번째도전 시간초과

--- 10818.java
+++ 10818.java
@@ -3,45 +3,42 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int max = -1000000;
+		int min = 1000000;
 		int n = sc.nextInt();
-		int[] array = new int[2000003];
+		int[] array = new int[n];
 
-		for (int i = 0; i < n; i++) {
-			int m = sc.nextInt();
-			if (m < 0) {
-				array[m + 1000001]++;
-			}
-			if (m > 0) {
-				array[m + 1000001]++;
-			}
-			if (m == 0) {
-				array[m + 1000001]++;
-			}
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.nextInt();
 		}
-		int start = 0, end = 2000002;
-		while (true) {
-			if (array[start] == 1) {
-				if (start == 1000001) {
-					System.out.print(0 + " ");
-					break;
+		for (int j = 0; j < array.length; j++) {
+			for (int k = j + 1; k < array.length; k++) {
+				if (array[j] < array[k]) {
+					if(max<array[k]) {
+					max = array[k];
+					}
 				}
-				if (array[start] < 0) {
-					System.out.print((start - 100000) + " ");
-					break;
-				}
-				if (array[start] > 0) {
-					System.out.print((start - 1000001) + " ");
-					break;
+				if (array[j] > array[k]) {
+					if(max<array[j]) {
+					max = array[j];
+					}
 				}
 			}
-			start++;
 		}
-		while (true) {
-			if (array[end] == 1) {
-				System.out.println((end - 1000001));
-				break;
-			}
-			end--;
-		}
+		for (int j = 0; j < array.length; j++) {
+			for (int k = j + 1; k < array.length; k++) {
+				if (array[j] > array[k]) {
+					if(min>array[k]) {
+					min = array[k];
+					}
+				}
+				if (array[j] < array[k]) {
+					if(min>array[j]) {
+					min = array[j];
+					}
+				}
+	}
+}
+		System.out.println(min+" "+max);
 	}
 }
