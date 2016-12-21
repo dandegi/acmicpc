5f1f7d3740ceca489f5c95328d354df090415e16
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2016-12-21 22:08:30 +0900

    나무조각 : 다른방법으로 바꾸어서

--- 2947.java
+++ 2947.java
@@ -4,35 +4,24 @@ import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		int [] array = new int[5];
-		put(array);
-		go(array);
-	}
-	public static void put(int [] array) throws IOException {
+		int[] array = new int[5];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String [] s = br.readLine().split(" ");
-		for(int i=0; i<5; i++) {
+		String[] s = br.readLine().split(" ");
+		for (int i=0; i<5; i++) {
 			array[i] = Integer.parseInt(s[i]);
 		}
-	}
-	public static void swap(int [] array) {
-			int i = 0;
-			for (int j=1; j<5; j++) {
-					if (array[i]>array[j]) {
-						int a = array[i];
-						array[i] = array[j];
-						array[j] = a;
-							System.out.println(array[0]+" "+array[1]+" "+array[2]+" "+array[3]+" "+array[4]);
-					}
-					i++;
+		for (int i = 0; i <5; i++) {
+			int a = 0;
+			for (int j = 1; j < 5-i; j++) {
+				if(array[a]>array[j]) {
+					int t = array[a];
+					array[a] = array[j];
+					array[j] = t;
+					System.out.print(array[0]+" "+array[1]+" "+array[2]+" "+array[3]+" "+array[4]);
+					System.out.println();
+				}
+				a++;
 			}
-	}
-	public static void go(int [] array) {
-		while (true) {
-			if(array[0]==1&&array[1]==2&&array[2]==3&&array[3]==4) {
-				break;
-			}
-			swap(array);
-			}
+		}
 	}
 }
