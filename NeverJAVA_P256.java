999250961e254bf8e76c7aa7ac91a94b641d80f2
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2016-12-25 18:18:50 +0900

    자바공부한적 없어요. 256페이지 문제 스스로해결 처음한것

---
+++ NeverJAVA_P256.java
@@ -0,0 +1,56 @@
+
+class Point {
+	int xPos, yPos;
+
+	public Point(int x, int y) {
+		xPos = x;
+		yPos = y;
+	}
+
+	public void showPointInfo() {
+		System.out.println("[" + xPos + "," + yPos + "]");
+	}
+}
+
+class Circle {
+	Point origin;
+	int radius;
+
+	public Circle(Point point, int r) {
+		origin = point;
+		radius = r;
+	}
+
+	public void showCircleInfo() {
+		System.out.println("반지름은 " + radius);
+		origin.showPointInfo();
+	}
+}
+
+class Ring {
+	Circle circle1;
+	Circle circle2;
+
+	public Ring(int x, int y, int r, int x2, int y2, int r2) {
+		Point po1 = new Point(x, y);
+		Point po2 = new Point(x2, y2);
+		Circle cir1 = new Circle(po1, r);
+		Circle cir2 = new Circle(po2, r2);
+		circle1 = cir1;
+		circle2 = cir2;
+	}
+
+	public void showRingInfo() {
+		System.out.println("Inner Circle Info...");
+		circle1.showCircleInfo();
+		System.out.println("Outter Circle Info...");
+		circle2.showCircleInfo();
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Ring ring = new Ring(1, 1, 4, 2, 2, 9);
+		ring.showRingInfo();
+	}
+}
