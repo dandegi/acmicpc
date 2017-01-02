f4d92007e8a74dbb341424d80a98a69047520bc0
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-01-03 00:37:54 +0900

    큐를 자바 내부 큐를 사용해서 구현

--- 10845.java
+++ 10845.java
@@ -1,92 +1,84 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-class Queue {
-	int[] que;
-	int fr, rear;
+class DequeClass {
+	Queue<Integer> qu;
 
-	public Queue(int x) {
-		que = new int[x];
-		fr = 0;
-		rear = -1;
+	public DequeClass() {
+		qu = new LinkedList<>();
 	}
 
-	void push(int x) {
-		rear++;
-		que[rear] = x;
+	public void push(int x) {
+		qu.add(x);
 	}
 
-	int pop() {
-		if(fr-1==rear) {
+	public int pop() {
+		if (size() == 0) {
 			return -1;
 		} else {
-			int a = que[fr];
-			que[fr] = 0;
-			fr++;
-			return a;
+			return qu.remove();
 		}
 	}
 
-	int size() {
-		int size = 0;
-		for(int i=0; i<que.length; i++) {
-			if(que[i]!=0) {
-				size++;
-			}
-		}
-		return size;
+	public int size() {
+		return qu.size();
 	}
 
-	int empty() {
-		if (fr - 1 == rear) {
+	public int empty() {
+		if (qu.isEmpty() == true) {
 			return 1;
 		} else {
 			return 0;
 		}
+
 	}
 
-	int front() {
-		if (fr - 1 == rear) {
+	public int front() {
+		if (size() == 0) {
 			return -1;
 		} else {
-			return que[fr];
+			return qu.peek();
 		}
 	}
 
-	int back() {
-		if (fr - 1 == rear) {
+	public int back() {
+		if (size() == 0) {
 			return -1;
-		} else {
-			return que[rear];
+		}
+		else {
+			return (int) qu.toArray()[qu.size()-1];
 		}
 	}
 }
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+		DequeClass dec = new DequeClass();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Queue qu = new Queue(n);
 		for (int i = 0; i < n; i++) {
 			String[] s = br.readLine().split(" ");
 			if (s[0].equals("push")) {
-				qu.push(Integer.parseInt(s[1]));
+				dec.push(Integer.parseInt(s[1]));
 			}
 			if (s[0].equals("pop")) {
-				System.out.println(qu.pop());
-			}
-			if (s[0].equals("size")) {
-				System.out.println(qu.size());
+				System.out.println(dec.pop());
 			}
 			if (s[0].equals("empty")) {
-				System.out.println(qu.empty());
+				System.out.println(dec.empty());
+			}
+			if (s[0].equals("size")) {
+				System.out.println(dec.size());
 			}
 			if (s[0].equals("front")) {
-				System.out.println(qu.front());
+				System.out.println(dec.front());
 			}
-			if (s[0].equals("back")) {
-				System.out.println(qu.back());
+			if(s[0].equals("back")) {
+				System.out.println(dec.back());
 			}
 		}
 	}
