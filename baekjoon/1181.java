4e12e8162c432cb14449dce5f2c949b1ea35b0d0
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-01-13 22:36:20 +0900

    단어 길이로 우선순위 매긴후 사전순으로 정리

--- baekjoon/1181.java
+++ baekjoon/1181.java
@@ -1,60 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<String> hset = new HashSet<String>();
 		int n = Integer.parseInt(br.readLine().trim());
-		ArrayList<Word> list = new ArrayList<Word>();
 		for(int i=0; i<n; i++) {
-			String s = br.readLine();
-			list.add(new Word(s));
+			hset.add(br.readLine());
 		}
-		for(int i=0; i<list.size(); i++) {
-			String temp = list.get(i).s;
-			for(int j=i+1; j<list.size(); j++) {
-				if(temp.equals(list.get(j).s)) {
-					list.remove(j);
-					j--;
-				}
-			}
-		}
-		Collections.sort(list, new WordSort());
+		String[] s = new String[hset.size()];
+		s = hset.toArray(s);
+		
+		Arrays.sort(s, new Sort());
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<list.size(); i++) {
-			sb.append(list.get(i).s+"\n");
+		for(int i=0; i<s.length; i++) {
+			sb.append(s[i]).append("\n");
 		}
 		System.out.println(sb);
 	}
 }
 
-class Word {
-	String s;
-	public Word(String s) {
-		this.s = s;
-	}
-	public String toString() {
-		return s;
-	}
-}
-class WordSort implements Comparator<Word> {
+class Sort implements Comparator<String> {
 
 	@Override
-	public int compare(Word o1, Word o2) {
+	public int compare(String o1, String o2) {
 		// TODO Auto-generated method stub
-		if(o1.s.length() < o2.s.length()) {
+		if(o1.length()<o2.length()) {
 			return -1;
-		} else if(o1.s.length() > o2.s.length()) {
+		} else if(o1.length()>o2.length()) {
 			return 1;
-		} else if(o1.s.length() == o2.s.length()){
-			return o1.s.compareTo(o2.s);
+		} else if(o1.length() == o2.length()) {
+			return o1.compareTo(o2);
 		} else {
 			return 0;
 		}
 	}
+	
 }
