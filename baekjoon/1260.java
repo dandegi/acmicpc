5768e3810a780e50b95818cb31c1b89bcbdb8992
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-01-20 21:10:30 +0900

    인접리스트로 DFS스택, BFS 큐로만들기 메소드따로 보기좋게 정리

--- baekjoon/1260.java
+++ baekjoon/1260.java
@@ -8,22 +8,25 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
-	
-	public static void dfs(ArrayList<ArrayList> a, int v, int st, StringBuilder sb) {
+	static StringBuilder sb = new StringBuilder();
+
+	public static void dfs(ArrayList<ArrayList<Integer>> a, boolean[] c, int start) {
 		Stack<Integer> stack = new Stack<>();
-		boolean[] c = new boolean[v + 1];
-		stack.push(st);
-		c[st] = true;
-		sb.append(st).append(" ");
+		stack.push(start);
+		c[start] = true;
+		sb.append(start).append(" ");
 		while (!stack.isEmpty()) {
 			int vv = stack.peek();
+			int size = a.get(vv).size();
 			boolean cc = false;
-			for (int i = 0; i < a.get(vv).size(); i++) {
-				if (c[(int) a.get(vv).get(i)] == false) {
-					stack.push((Integer) a.get(vv).get(i));
-					c[(int) a.get(vv).get(i)] = true;
+			
+			for (int i = 0; i < size; i++) {
+				int v = (int) a.get(vv).get(i);
+				if (c[v] == false) {
+					stack.push(v);
+					c[v] = true;
 					cc = true;
-					sb.append(a.get(vv).get(i)).append(" ");
+					sb.append(v).append(" ");
 					break;
 				}
 			}
@@ -32,36 +35,36 @@ public class Main {
 			}
 		}
 	}
-	
-	public static void bfs(ArrayList<ArrayList> a, int v, int st, StringBuilder sb) {
+
+	public static void bfs(ArrayList<ArrayList<Integer>> a, boolean[] c, int start) {
 		Queue<Integer> q = new LinkedList<>();
-		boolean[] c = new boolean[v+1];
-		q.offer(st);
-		c[st] = true;
-		sb.append(st).append(" ");
-		while(!q.isEmpty()) {
+		q.offer(start);
+		c[start] = true;
+		sb.append(start).append(" ");
+		while (!q.isEmpty()) {
 			int vv = q.poll();
+			int size = a.get(vv).size();
 			
-			for(int i=0; i<a.get(vv).size(); i++) {
-				if(c[(int)a.get(vv).get(i)] == false) {
-					q.offer((int)a.get(vv).get(i));
-					c[(int)a.get(vv).get(i)] = true;
-					sb.append(a.get(vv).get(i)).append(" ");
+			for (int i = 0; i < size; i++) {
+				int v = (int) a.get(vv).get(i); 
+				if (c[v] == false) {
+					q.offer(v);
+					c[v] = true;
+					sb.append(v).append(" ");
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		String[] s = br.readLine().split(" ");
 		int v = Integer.parseInt(s[0]);
 		int e = Integer.parseInt(s[1]);
-		int st = Integer.parseInt(s[2]);
-		
-		ArrayList<ArrayList> a = new ArrayList<ArrayList>();
-		
+		int start = Integer.parseInt(s[2]);
+		boolean[] c = new boolean[v + 1];
+		ArrayList<ArrayList<Integer>> a = new ArrayList<ArrayList<Integer>>();
+
 		for (int i = 0; i <= v; i++) {
 			a.add(new ArrayList<Integer>());
 		}
@@ -73,14 +76,15 @@ public class Main {
 			a.get(n).add(m);
 			a.get(m).add(n);
 		}
-		
-		for(int i=0; i<a.size(); i++) {
+
+		for (int i = 0; i < a.size(); i++) {
 			Collections.sort(a.get(i));
 		}
-		
-		dfs(a, v, st, sb);
+
+		dfs(a, c, start);
 		sb.append("\n");
-		bfs(a, v, st, sb);
+		c = new boolean[v + 1];
+		bfs(a, c, start);
 		System.out.println(sb);
 	}
 }
