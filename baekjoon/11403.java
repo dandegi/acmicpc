fa6e0feea37c9756d11038df49ed265f75eccd24
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-01-31 14:43:58 +0900

    방향있는 그래프 인접행렬 경로찾기 DFS로 만듬

--- baekjoon/11403.java
+++ baekjoon/11403.java
@@ -1,25 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
-	public static void bfs(int[][] a, boolean[] c, int start, int v, int[][] ans) {
-		Queue<Integer> qu = new LinkedList<Integer>();
-		qu.offer(start);
-		while (!qu.isEmpty()) {
-			int vv = qu.poll();
-
+	
+	public static void dfs(int[][] a, boolean[] c, int start, int v, int[][] ans) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(start);
+		while (!stack.isEmpty()) {
+			boolean cc = false;
+			int vv = stack.peek();
 			for (int i = 1; i <= v; i++) {
 				if (a[vv][i] == 1 && c[i] == false) {
-					ans[start][i] = 1;
+					cc = true;
+					stack.push(i);
 					c[i] = true;
-					qu.offer(i);
+					ans[start][i] = 1;
+					break;
 				}
+			}
+			if (cc == false) {
+				stack.pop();
 			}
 		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int v = Integer.parseInt(br.readLine());
@@ -35,7 +41,7 @@ public class Main {
 
 		for (int i = 1; i <= v; i++) {
 			c = new boolean[v+1];
-			bfs(a, c, i, v, ans);
+			dfs(a, c, i, v, ans);
 		}
 		StringBuilder sb = new StringBuilder();
 		for(int i=1; i<=v; i++) {
