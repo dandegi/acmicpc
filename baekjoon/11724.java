f582c07364948757a48f83787fcf63e12892a5ea
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-02-01 01:34:17 +0900

    그래프 연결요소 찾기 재귀 dfs 인접리스트 사용

--- baekjoon/11724.java
+++ baekjoon/11724.java
@@ -1,38 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
+	static StringBuilder sb = new StringBuilder();
 
-	public static void dfs(int[][] a, boolean[] c, int v) {
-
-		int n = a.length - 1;
-		c[v] = true;
-		for (int i = 1; i <= n; i++) {
-			if (a[v][i] == 1 && !c[i]) {
-				dfs(a, c, i);
+	public static void dfs(ArrayList<Integer>[] a, boolean[] c, int start) {
+		if (c[start]) {
+			return;
+		}
+		c[start] = true;
+		// for (int vv : a[start]) {
+		//
+		// if (!c[vv]) {
+		// dfs(a, c, vv);
+		// }
+		//
+		// }
+		int size = a[start].size();
+		for (int i = 1; i <= size; i++) {
+			if (c[a[start].get(i - 1)] == false) {
+				dfs(a, c, a[start].get(i - 1));
 			}
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int cnt = 0;
 		String[] s = br.readLine().split(" ");
 		int n = Integer.parseInt(s[0]);
 		int m = Integer.parseInt(s[1]);
-		int[][] a = new int[n + 1][n + 1];
+		int cnt = 0;
 		boolean[] c = new boolean[n + 1];
-		for (int i = 0; i < m; i++) {
+		ArrayList<Integer>[] a = (ArrayList<Integer>[]) new ArrayList[n + 1];
+		for(int i=0; i<=n; i++) {
+			a[i] = new ArrayList<>();
+		}
+		for (int j = 1; j <= m; j++) {
 			String[] ss = br.readLine().split(" ");
 			int v = Integer.parseInt(ss[0]);
 			int vv = Integer.parseInt(ss[1]);
-			a[v][vv] = 1;
-			a[vv][v] = 1;
+			a[v].add(vv);
+			a[vv].add(v);
 		}
-		for (int i = 1; i <= n; i++) {
-			if (c[i] == false) {
-				dfs(a, c, i);
+		for (int j = 1; j <= n; j++) {
+			if (c[j] == false) {
+				dfs(a, c, j);
 				cnt++;
 			}
 		}
