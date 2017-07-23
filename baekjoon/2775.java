bb82cb122fa03b838f764d6803228958e55fac46
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-07-23 18:53:00 +0900

    2775

---
+++ baekjoon/2775.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	
+	public static void main(String args[]) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		int[][] arr = new int[15][15];
+		
+		for(int i=0; i<15; i++) {
+			arr[0][i] = i;
+		}
+		for(int i=0; i<15; i++) {
+			arr[i][1] = 1;
+		}
+		
+		for(int i=1; i<15; i++) {
+			
+			for (int j=2; j<15; j++) {
+				
+				arr[i][j] = arr[i][j-1] + arr[i-1][j];
+				
+			}
+			
+		}
+		
+		while(t-- > 0) {
+			
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			
+			sb.append(arr[k][n]).append("\n");
+			
+		}
+		
+		System.out.print(sb);
+		
+	}
+	
+}
