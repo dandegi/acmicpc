c72d9f6dbb9a6d4cd752ea39c7a8dc5228534124
김재진 <JaeJin@gimjaejin-ui-MacBook-Pro.local>
2017-08-24 18:07:02 +0900

    최소 비행기 숫자

---
+++ baekjoon/9372.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Edge {
+	int x, y;
+
+	public Edge(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			String[] s = br.readLine().split(" ");
+			int n = Integer.parseInt(s[0]);
+			int m = Integer.parseInt(s[1]);
+			int[] parent = new int[n + 1];
+			int minCost = 0;
+
+			Edge[] e = new Edge[m];
+
+			for (int i = 1; i <= n; i++) {
+				parent[i] = i;
+			}
+
+			for (int i = 0; i < m; i++) {
+				String[] ss = br.readLine().split(" ");
+				e[i] = new Edge(Integer.parseInt(ss[0]), Integer.parseInt(ss[1]));
+			}
+			
+			for (int i = 0; i < m; i++) {
+				int rootx = find(e[i].x, parent);
+				int rooty = find(e[i].y, parent);
+
+				if (rootx == rooty) {
+					continue;
+				} else {
+					parent[rootx] = rooty;
+					minCost++;
+				}
+			}
+			System.out.println(minCost);
+		}
+
+	}
+
+	private static int find(int x, int[] parent) {
+		if (x == parent[x]) {
+			return x;
+		} else {
+			parent[x] = find(parent[x], parent);
+			return parent[x];
+		}
+	}
+}
